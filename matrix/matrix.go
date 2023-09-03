23517ac982a8f65af227058b5677cb530d6ded36
Oscar Johansson Elling <oscar.johansson.elling@gmail.com>
2023-09-03 21:39:17 +0200

    enkel matris tänkt till skogen

---
+++ matrix/matrix.go
@@ -0,0 +1,48 @@
+package matrix
+
+import (
+	"errors"
+)
+
+type Matrix struct {
+	nrRows int
+	nrCols int
+	values []int
+}
+
+func Init(nrRows int, nrCols int) Matrix {
+	var values []int = []int{}
+	i := 0
+	for i < nrRows*nrCols {
+		values = append(values, 0)
+		i++
+	}
+	return Matrix{nrRows: nrRows, nrCols: nrCols, values: values}
+}
+
+func (this *Matrix) index(row int, col int) int {
+	return row*this.nrCols + col
+}
+
+func (this *Matrix) within(row int, col int) bool {
+	var inRows bool = 0 <= row && row < this.nrRows
+	var inCols bool = 0 <= col && col < this.nrCols
+	return inRows && inCols
+}
+
+func (this *Matrix) Set(row int, col int, v int) error {
+	if this.within(row, col) {
+		index := this.index(row, col)
+		this.values[index] = v
+		return nil
+	}
+	return errors.New("Set of coordinate outside matrix")
+}
+
+func (this *Matrix) Get(row int, col int) (int, error) {
+	if this.within(row, col) {
+		index := this.index(row, col)
+		return this.values[index], nil
+	}
+	return 0, errors.New("Get of coordinate outside matrix")
+}
---
+++ matrix/matrix_test.go
@@ -0,0 +1,35 @@
+package matrix
+
+import (
+	"testing"
+)
+
+func TestSetGet(t *testing.T) {
+	var m Matrix = Init(2, 4)
+	standardV, error1 := m.Get(0, 0)
+	if error1 != nil {
+		t.Errorf("error at get (0,0)")
+	}
+	if standardV != 0 {
+		t.Errorf("not standard value at (0,0)")
+	}
+	error2 := m.Set(1, 2, 10)
+	if error2 != nil {
+		t.Errorf("error with set within matrix")
+	}
+	v, error3 := m.Get(1, 2)
+	if error3 != nil {
+		t.Errorf("error when ger within matrix")
+	}
+	if v != 10 {
+		t.Errorf("set get loop failed")
+	}
+}
+
+func TestGetError(t *testing.T) {
+	var m Matrix = Init(0, 0) //empty matrix
+	v, error := m.Get(0, 0)
+	if v != 0 || error == nil {
+		t.Errorf("no error raised when accesing an empty matrix")
+	}
+}
